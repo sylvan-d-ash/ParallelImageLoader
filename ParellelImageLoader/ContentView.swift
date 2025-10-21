863fd19912d1d4f87555dbde241afb209c6a8e2c
Sylvan Ash <sylvan.d.ash@gmail.com>
2025-10-21 15:33:28 +0300

    Initial Commit

---
+++ ParellelImageLoader/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParellelImageLoader
+//
+//  Created by Sylvan  on 21/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParellelImageLoader/ParellelImageLoaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParellelImageLoaderApp.swift
+//  ParellelImageLoader
+//
+//  Created by Sylvan  on 21/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ParellelImageLoaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
